200f4b7509ec477e91062cb7f3b655ac1ae7984a
Vincenzo <vincenzo@localhost.localdomain>
2021-09-01 20:06:49 +0200

    Fix indicator type detection when selecting text

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [0.4.1] - 2021-09-01
 ### Fixed
 - Hiding of web resources list when the text field is empty
+- Indicator type detection when selecting text
 
 ## [0.4.0] - 2021-08-31
 ### Added
--- addon/src/content_scripts/context_menu.js
+++ addon/src/content_scripts/context_menu.js
@@ -5,12 +5,12 @@ var lastType = "";
  * Selection change event
  */
 document.addEventListener("selectionchange", () => {
-    indicatorTypeDetector = new IndicatorTypeDetector();
+    indicatorParser = new IndicatorParser();
     selectedText = document.getSelection().toString().trim();
     if(selectedText) {
         // Determine the type of the indicator selected
 
-        type = indicatorTypeDetector.getIndicatorType(selectedText);
+        type = indicatorParser.getIndicatorType(selectedText);
         console.log("Type: "+type);
         if(type != "invalid"){
             // Send the selected text to background script along with its type
