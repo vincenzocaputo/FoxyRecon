b5e6564056abcae01bedda0db5e0ec0b246784ec
Vincenzo <32276363+vincenzocaputo@users.noreply.github.com>
2024-10-20 21:11:49 +0200

    Migrate to storage API

--- src/background.js
+++ src/background.js
@@ -1,21 +1,22 @@
 // Setup default settings
-if (!localStorage.getItem("settings.newtab")) {
-    localStorage.setItem("settings.newtab", "true");
-}
-if (!localStorage.getItem("settings.autosubmit")) {
-    localStorage.setItem("settings.autosubmit", "false");
-}
-if (!localStorage.getItem("settings.autocatch")) {
-    localStorage.setItem("settings.autocatch", "false");
-}
-if (!localStorage.getItem("settings.autograph")) {
-    localStorage.setItem("settings.autograph", "false");
+browser.storage.local.get("settings").then( (s) => {
+    console.log("Settings loaded");
+    if (!s.newtab) {
+        s.newtab
+        browser.storage.local.set({"settings.newtab": "true"});
+    }
+}, (err) => {
+    console.log("Setting default settings");
+    const defaultSettings = {
+        newtab: true,
+        autosubmit: true,
+        autocatch: true,
+        autograph: true
+        typeanim: true
+    }
+    browser.storage.local.set({"settings": settings});
+} );
 
-}
-if (!localStorage.getItem("settings.typanim")) {
-    localStorage.setItem("settings.typanim", "true");
-
-}
 var tools;
 loadToolsList(function(ts) {
     tools=ts;
@@ -28,44 +29,49 @@ loadGraphMapping(function(mp) {
 
 browser.runtime.onInstalled.addListener(function(details) {
     let currentVersion = browser.runtime.getManifest().version;
-    if(details.reason === "install") {
-        console.log("Current version: " + currentVersion);
-        localStorage.setItem("version", currentVersion);
-    } else if(details.reason === "update") {
-        if (installedVersion = localStorage.getItem("version")) {
-            console.log("Installed version: " + installedVersion);
-        } else {
-            // Assume that the installed version is obsolete
-            installedVersion = 0;
+    if(details.reason === "install" || details.reasson === "update") {
+        if (details.reason === "instal") {
+            console.log("Current version: " + currentVersion);
+            browser.storage.local.set({"version": currentVersion});
+        } else if(details.reason === "update") {
+            browser.storage.local.get("version")
+                .then( (installedVersion) => {
+                    console.log("Installed version: " + installedVersion);
+                    return installedVersion;
+                }, () => {
+                    // Assume that the installed version is obsolete
+                    installedVersion = 0;
+                    return installedVersion;
+                })
+                .then( (installedVersion) => {
+                    if (installedVersion !== currentVersion) {
+                        browser.tabs.create({
+                            discarded: true,
+                            title: "FoxyRecon New Version",
+                            url: 'https://github.com/vincenzocaputo/FoxyRecon/releases/tag/v'+currentVersion
+                        });
+                        // If a new version was released, clean the local storage
+                        browser.storage.local.remove("tools");
+                        browser.storage.local.remove("type");
+                        browser.storage.local.remove("tag");
+                        browser.storage.local.remove("autograph-mapping");
+                        browser.storage.local.remove("submit-btn-query");
+                        browser.storage.local.remove("autofill.submit-btn-query");
+                        browser.storage.local.remove("autofill.input-selector");
+                        browser.storage.local.remove("indicator");
+                        // Add current version to local storage/
+                        browser.storage.local.set({"version": currentVersion});
+                    }
+                });
         }
-        
-        if (installedVersion !== currentVersion) {
-            browser.tabs.create({
-                discarded: true,
-                title: "FoxyRecon New Version",
-                url: 'https://github.com/vincenzocaputo/FoxyRecon/releases/tag/v'+currentVersion
-            });
-            // If a new version was released, clean the local storage
-            localStorage.removeItem("tools");
-            localStorage.removeItem("type");
-            localStorage.removeItem("tag");
-            localStorage.removeItem("autograph-mapping");
-            localStorage.removeItem("submit-btn-query");
-            localStorage.removeItem("autofill.submit-btn-query");
-            localStorage.removeItem("autofill.input-selector");
-            localStorage.removeItem("indicator");
-            // Add current version to local storage
-            localStorage.setItem("version", currentVersion);
+        var tools;
+        loadToolsList(function(ts) {
+            tools=ts;
+        })
 
-            var tools;
-            loadToolsList(function(ts) {
-                tools=ts;
-            })
-
-            var graphMapping;
-            loadGraphMapping(function(mp) {
-                graphMapping=mp;
-            })
+        var graphMapping;
+        loadGraphMapping(function(mp) {
+            graphMapping=mp;
         }
     }
     browser.contextMenus.create({
@@ -94,12 +100,14 @@ browser.runtime.onInstalled.addListener(function(details) {
                         // If the input string is defanged, refang it
                         selectionText = indicatorParser.refangIndicator(selectionText);
                         [type, tld] = indicatorParser.getIndicatorType(selectionText);
-
                     }
-                    localStorage.setItem("type", type);
-                    localStorage.setItem("indicator", selectionText);
-                    localStorage.setItem("tld", tld);
-                    browser.action.openPopup();
+                    browser.storage.local.set({"indicator": {
+                        "type": type,
+                        "value": selectionText,
+                        "tld": tld
+                    }).then( () => {
+                        browser.action.openPopup();
+                    });
                 } else {
                     browser.tabs.query({active:true, lastFocusedWindow: true}).then(tabs => {    
                         let activeTab = tabs[0].id;
@@ -142,42 +150,44 @@ browser.runtime.onInstalled.addListener(function(details) {
  * Harvest and collect the indicators present in the current webpage. Save the list in the local storage.
  */
 function catchIndicators(e) {
-    let autocatch_option = localStorage.getItem("settings.autocatch");
-    if (autocatch_option && autocatch_option === "true") {
-        browser.tabs.query({active:true, lastFocusedWindow: true}).then(tabs => {    
-            let activeTab = tabs[0].id;
-            // Send a message to the content script
-            browser.tabs.sendMessage(activeTab, "catch")
-                        .then((response) => {
-                        })
-                        .catch((error) => {
+    browser.storage.local.get("settings").then( (settings) => {
+        const autocatch_option = settings.autocatch;
+        if (autocatch_option && autocatch_option === "true") {
+            browser.tabs.query({active:true, lastFocusedWindow: true}).then(tabs => {    
+                let activeTab = tabs[0].id;
+                // Send a message to the content script
+                browser.tabs.sendMessage(activeTab, "catch")
+                            .then((response) => {
+                            })
+                            .catch((error) => {
+                                browser.action.setBadgeText({text: ""});
+                                browser.storage.local.set({"catched_indicators": "[]"});
+                            });
+                let token = 1;
+                browser.runtime.onMessage.addListener(function(message) {
+                    if(token) {
+                        const indicatorsListJson = message['indicators'];
+                        // No indicators found. Show a message
+                        if(indicatorsListJson == "[]") {
                             browser.action.setBadgeText({text: ""});
-                            localStorage.setItem("catched_indicators", "[]");
-                        });
-            let token = 1;
-            browser.runtime.onMessage.addListener(function(message) {
-                if(token) {
-                    const indicatorsListJson = message['indicators'];
-                    // Save the indicators list in the local storage
-                    localStorage.setItem("catched_indicators", indicatorsListJson);
-                    // No indicators found. Show a message
-                    if(indicatorsListJson == "[]") {
-                        browser.action.setBadgeText({text: ""});
-                    } else {
-                        const indicatorsList = JSON.parse(indicatorsListJson);
-                        browser.action.setBadgeText({text: indicatorsList.length.toString()});
-                    }
-                } 
-                // Consume token
-                token = 0;
-            })
-        },
-        error => {
-            browser.action.setBadgeText({text: ""});
-            localStorage.setItem("catched_indicators", "[]");
-            console.error("Error: "+error);
-        });
-    }
+                        } else {
+                            const indicatorsList = JSON.parse(indicatorsListJson);
+                            browser.action.setBadgeText({text: indicatorsList.length.toString()});
+                        }
+                        // Save the indicators list in the local storage
+                        browser.storage.local.set({"catched_indicators": indicatorsListJson});
+                    } 
+                    // Consume token
+                    token = 0;
+                })
+            },
+            error => {
+                browser.action.setBadgeText({text: ""});
+                browser.storage.local.set({"catched_indicators": "[]"});
+                console.error("Error: "+error);
+            });
+        }
+    });
 }
 /**
  * When tab changes, remove the badge text
@@ -192,27 +202,47 @@ browser.tabs.onCreated.addListener(catchIndicators);
 browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if(request.id === 1) {
         // Autofill feature
-        query = localStorage.getItem("autofill.submit-btn-query");
-        inputSelector = localStorage.getItem("autofill.input-selector");
-        submit = localStorage.getItem("settings.autosubmit");
-        typAnimOption = localStorage.getItem("settings.typanim");
+        let query; // The CSS selector of the submit button
+        let inputSelector; // The CSS selector of the input field
+        let submit; // Defines if the input must be automatically submitted
+        let typAnimOption; // True for typing animation
 
-        sendResponse({msg: localStorage.getItem("indicator"), query: query, inputSelector: inputSelector, submit: submit, typAnimOption: typAnimOption});
-        // Consume the request (to avoid clicking the button more times for the same request)
-        localStorage.setItem("autofill.submit-btn-query","");
-        localStorage.setItem("autofill.input-selector","");
+        browser.storage.local.get("autofill")
+            .then( (autofill) => {
+                query = autofill.submitQuery;
+                inputSelector = autofill.inputSelector;
+                return browser.storage.local.get("settings");
+            })
+            .then( (settings) => {
+                submit = settings.autosubmit;
+                typAnimOption = settings.typenanim;
+                return browser.storage.local.get("indicator");
+            })
+            .then( (indicator) => {
+                sendResponse({msg: indicator.value, query: query, inputSelector: inputSelector, submit: submit, typAnimOption: typAnimOption});
+                // Consume the request (to avoid clicking the button more times for the same request)
+                browser.storage.local.set({ "autofill": {
+                    submitQuery: "";
+                    inputSelector: "";
+                });
+            });
     } else if (request.id === 2) {
         // Auto graph generation
-        if (localStorage.getItem("settings.autograph") === "true" && request.msg) {
-            const resource = request.msg;
-            let mappings = Array();
-            for (i=0; i<graphMapping.length; i++) {
-                if (resource.startsWith(graphMapping[i]['source'])) {
-                    mappings.push(graphMapping[i]);
+        browser.storage.local.get("settings").then( (settings) => {
+            if (settings.autograph && request.msg) {
+                const resource = request.msg;
+                let mappings = Array();
+                for (i=0; i<graphMapping.length; i++) {
+                    if (resource.startsWith(graphMapping[i]['source'])) {
+                        mappings.push(graphMapping[i]);
+                    }
                 }
+                return browser.storage.local.get("indicator");
             }
-            sendResponse({msg: localStorage.getItem("indicator"), map: JSON.stringify(mappings) }) 
-        }
+        })
+        .then( (indicator) => {
+            sendResponse({msg: indicator.value, map: JSON.stringify(mappings) }) 
+        });
     } else if (request.id === 3) {
         const graph = new Graph();
         const rel = JSON.parse(request.msg);
@@ -249,22 +279,22 @@ browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             nodeId = stix["id"];
         }
         if (request.relName) {
-            const indicator = localStorage.getItem("indicator");
-            const nodes = graph.getNodesByLabel(indicator);
+            browser.storage.local.get("indicator").then( (indicator) => {
+                const nodes = graph.getNodesByLabel(indicator.value);
 
-            let relNodeId;
-            if (nodes.length === 0) {
-                relNodeId = graph.addNode(indicator, localStorage.getItem("type"));
-            } else {
-                relNodeId = nodes[0];
-            }
-            if (request.inbound) {
-                graph.addLink(relNodeId, nodeId, request.relName);
-            }
-            if (request.outbound) {
-                graph.addLink(nodeId, relNodeId, request.relName);
-            }
-                
+                let relNodeId;
+                if (nodes.length === 0) {
+                    relNodeId = graph.addNode(indicator.value, indicator.type);
+                } else {
+                    relNodeId = nodes[0];
+                }
+                if (request.inbound) {
+                    graph.addLink(relNodeId, nodeId, request.relName);
+                }
+                if (request.outbound) {
+                    graph.addLink(nodeId, relNodeId, request.relName);
+                }
+            });
         }
         browser.tabs.query({active:true, lastFocusedWindow: true}).then(tabs => {    
             let activeTab = tabs[0].id;
